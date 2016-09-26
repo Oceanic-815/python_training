df42916f3e2cda72af7c6562c0fc8ebf15098593
Alexander Krasnov <rn4acy@mail.ru>
2016-09-26 16:05:11 +0300

    Задание 3 выполнено
    Проблема с логином пофикшена. Причина - перепутал местами параметры
    password и username в методе login

--- test_add_new_contact.py
+++ test_add_new_contact.py
@@ -126,10 +126,10 @@ class test_add_new_contact(unittest.TestCase):
     def login(self, wd, username, password):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(username)
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(password)
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
