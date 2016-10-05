1790b75512f38341fb7c2d2b5d61759ac1c05af9
Alexander Krasnov <rn4acy@mail.ru>
2016-10-05 12:27:51 +0300

    Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,11 +14,11 @@ def app(request):
 def test_add_group(app):
     #success = True
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="3333", header="3333", footer="3333"))
+    app.group.create(Group(name="3333", header="3333", footer="3333"))
     app.session.logout()
 
 #***>> added manually to check behaviour with empty fields <<***
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
