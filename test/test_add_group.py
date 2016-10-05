f30418ee019c27196536e76d749a5d97f74c0f2f
Alexander Krasnov <rn4acy@mail.ru>
2016-10-05 19:38:41 +0300

    Общая фикстура для всех тестов создана (Задание 6 выполнено)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.group import Group
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 def test_add_group(app):
-    #success = True
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="3333", header="3333", footer="3333"))
     app.session.logout()
