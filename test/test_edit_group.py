69c719d4a6cb6b7892fe681ee9b56f8d2fce0c3b
Alexander Krasnov <rn4acy@mail.ru>
2016-10-10 16:34:50 +0300

    Занятие 3: Оптимизация ввода данных в формы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,4 +1,11 @@
+from model.group import Group
+
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group()
+    app.group.edit_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header = "New header"))
     app.session.logout()
