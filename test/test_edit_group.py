4b181f9609e9c969abe02acb16534589058798c5
Alexander Krasnov <rn4acy@mail.ru>
2016-10-24 21:13:20 +0300

    Добавил метод получения списка групп (Занятие 4.1)

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,11 +2,17 @@
 from model.group import Group
 
 def test_edit_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="just added", header="just added", footer="just added"))
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_first_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="just added", header="just added", footer="just added"))
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
