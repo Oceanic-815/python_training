defa433a45652453f8e7e7226bec0557b3aa0874
Alexander Krasnov <rn4acy@mail.ru>
2016-11-15 19:32:26 +0300

    Задание 12 выполнено

--- fixture/contact.py
+++ fixture/contact.py
@@ -203,6 +203,8 @@ class ContactHelper:
         wd.find_element_by_name("notes").send_keys("Edited!!!")
             #saving contact
         wd.find_element_by_name("update").click()
+        # Clean up cache
+        self.contact_cache = None
 
     def del_contact(self):
         wd = self.app.wd
@@ -215,6 +217,8 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         wd.find_element_by_link_text("home").click()
+        # Clean up cache
+        self.contact_cache = None
 
         # define if there is any contact in the contacts list
     def count_cont(self):
