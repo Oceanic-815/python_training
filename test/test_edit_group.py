463e43a5e6464871dbc1444bab4000b562d71022
Alexander Krasnov <rn4acy@mail.ru>
2016-11-16 19:06:46 +0300

    Добавил выбор случайных груп для удаления и редактирования

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 def test_edit_first_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="New group", header="added header", footer="added footer")
     if app.group.count() == 0:
-        app.group.create(group)#(Group(name="just added", header="just added", footer="just added"))
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+        app.group.create(group)
+    index = randrange(len(old_groups))
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 """
